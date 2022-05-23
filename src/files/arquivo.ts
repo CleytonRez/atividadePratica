c72c581872163b9429af4d184fbc680206f40fe2
Cleyton Rezende <cleytonmgrezendedev@gmail.com>
2022-05-23 18:16:10 -0300

    feature: Update Projet

---
+++ src/files/arquivo.ts
@@ -0,0 +1,36 @@
+const Arquivo = () => {
+ const listaAlunos =  [
+    {
+        name: "Cleyton Eduardo Rezende",
+        ru: 3635113,
+        curso: "Engenharia de Software",
+        dataNascimento: "24/08/2000"
+    },
+    {
+        name: "Oscar Geraldo",
+        ru: 1212122,
+        curso: "Engenharia",
+        dataNascimento: "10/08/1983"
+    },
+    {
+        name: "Kaike",
+        ru: 302020,
+        curso: "Artes Visuais",
+        dataNascimento: "02/10/2000"
+    },
+    {
+        name: "Jorge Dimas",
+        ru: 10101010,
+        curso: "Engenharia de Software",
+        dataNascimento: "24/08/1900"
+    },
+    {
+        name: "Geraldão",
+        ru: 2344567,
+        curso: "Segurança",
+        dataNascimento: "14/08/1999"
+    }
+];
+}
+
+export default Arquivo
